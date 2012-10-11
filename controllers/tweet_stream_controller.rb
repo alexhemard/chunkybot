58d59f51fe68ec4f2aa9954a181956eed6709ddd
Alex Hemard <alexhemard@gmail.com>
2012-10-11 13:00:16 -0500

    restart tweeter when follow/unfollow bros

--- controllers/tweet_stream_controller.rb
+++ controllers/tweet_stream_controller.rb
@@ -5,6 +5,10 @@ require 'tweetstream'
 class TweetStreamController < Rubot::Controller
 
   command :tweetstream do
+    response_callback = lambda do |message|
+      reply "\u0002@#{message.user.screen_name}: \u0002\u0016#{message.text}"
+    end
+    
     case
     when message.text.match(/^following/)
       screen_names = Tweeple.screen_names
@@ -12,26 +16,28 @@ class TweetStreamController < Rubot::Controller
     when follow = message.text.sub!(/^follow/, '')
       if user = TweetStreamer.instance.get_user(follow)
         Tweeple.find_or_create(:twitter_id => user.id, :screen_name => user.screen_name)
+        TweetStreamer.instance.start(&response_callback)
         reply "Following #{Tweeple.screen_names.join(', ')}"
       else
         reply "User #{follow} not found."
       end
     when message.text.match(/^unfollow_all/)
       Tweeple.dataset.delete
+      TweetStreamer.instance.start(&response_callback)
       reply "UNFOLLOWING ALL FOLLOWBROS"
     when remove = message.text.sub!(/^unfollow/, '')
       if user = TweetStreamer.instance.get_user(remove)
         Tweeple[user.id].destroy
+        TweetStreamer.instance.start(&response_callback)
         reply "Unfollowed #{remove.strip}."
       end
     when message.text.match(/^start/)
-      TweetStreamer.instance.start do |message|
-        reply "\u0002@#{message.user.screen_name}: \u0002\u0016#{message.text}"
-      end
+      TweetStreamer.instance.start(&response_callback)
       reply "Tweeter Starting..."
     when message.text.match(/^stop/)
       TweetStreamer.instance.stop
       reply "Tweeter Stopping..."
     end
-  end  
+  end
+  
 end
